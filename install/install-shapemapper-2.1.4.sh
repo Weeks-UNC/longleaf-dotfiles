fda58487d874608c68c21263f8fac3d1b7d9ea92
Patrick Irving <psirving@gmail.com>
2018-12-09 12:56:34 -0500

    Added install script for newest shapemapper2 release

---
+++ install/install-shapemapper-2.1.4.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# To run this script, use:
+# source install-shapemapper-2.1.3.sh
+
+START_DIR=$(pwd)
+builtin cd $HOME
+
+echo 'Downloading shapemapper...'
+curl -O 'https://github.com/Weeks-UNC/shapemapper2/releases/download/2.1.4/shapemapper-2.1.4.tar.gz'
+
+echo 'Extracting shapemapper...'
+tar xzf shapemapper-2.1.4.tar.gz
+rm shapemapper-2.1.4.tar.gz
+
+export PATH="$PATH:~/shapemapper-2.1.4/"
+echo 'export PATH="$PATH:~/shapemapper-2.1.3/"' >> .bash_$USER
+echo 'Shapemapper added to PATH, installation successful'
+
+builtin cd $START_DIR
