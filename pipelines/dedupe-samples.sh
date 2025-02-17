264760eb74dda0da49d99697ac079f3750964425
Patrick Irving <psirving@gmail.com>
2025-02-17 13:15:59 -0500

    fix fastq pattern in dedupe pipeline

--- pipelines/dedupe-samples.sh
+++ pipelines/dedupe-samples.sh
@@ -10,6 +10,13 @@ module load bbmap
 module load umi_tools
 
 cd Sample_$SLURM_ARRAY_TASK_ID
-bbmerge.sh in1=*R1_001.fastq.gz in2=*R2_001.fastq.gz out=out.extendedFrags.fastq outu1=out.unmerged1.fastq outu2=out.unmerged2.fastq
+
+pattern1="*R1_001.fastq.gz"
+pattern2="*R2_001.fastq.gz"
+fastq1=( $pattern1 )
+fastq2=( $pattern2 )
+echo ${fastq1[0]} ${fastq2[0]}
+
+bbmerge.sh in1=${fastq1[0]} in2=${fastq2[0]} out=out.extendedFrags.fastq outu1=out.unmerged1.fastq outu2=out.unmerged2.fastq
 umi_tools extract --extract-method=regex --bc-pattern='(?P<umi_1>.{5}).*(?P<umi_2>.{5})' -I out.extendedFrags.fastq -S combined_trimmed.fastq
 dedupe.sh in=combined_trimmed.fastq out=combined_trimmed_deduped.fastq ac=f
